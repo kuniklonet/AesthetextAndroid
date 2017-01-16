0824198defb5d2e6a5c2ef213e48ca94cef5a137
Mark Fisher <markisaacfisher@gmail.com>
2017-01-16 14:52:12 +1000

    Fix issues with landscape orientation

--- app/src/main/java/net/kuniklo/aesthetext/MainActivity.java
+++ app/src/main/java/net/kuniklo/aesthetext/MainActivity.java
@@ -87,6 +87,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+        spacingText.setText(Integer.toString(spacingBar.getProgress()));
     }
 
     @Override
